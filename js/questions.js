5bbde46863bbda834da692608f408f78a8d6139b
AbinEast <156790296+AbinEast@users.noreply.github.com>
2024-01-16 17:44:15 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Siapa Presiden Ke - 1 RI?",
+    answer: "Soekarno",
+    options: [
+      "Soeharto",
+      "Soekarno",
+      "Megawati",
+      "BJ Habibi"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Apa nama ibu kota indonesia?",
+    answer: "Jakarta",
+    options: [
+      "Bandung",
+      "Yogyakarta",
+      "Jakarta",
+      "Surabaya"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Apa Lambang Negara Indonesia?",
+    answer: "Garuda Pancasila",
+    options: [
+      "Garuda Pancasila",
+      "Merah Putih",
+      "Banteng",
+      "Rupiah"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Tanggal Berapa Indonesia Merdeka?",
+    answer: "17 Agustus 1945",
+    options: [
+      "17 Agustus 1944",
+      "16 Agustus 1945",
+      "17 Agustus 1945",
+      "16 Agustus 1944"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Bentuk Pemerintahan di Indonesia Adalah",
+    answer: "Republik Presidensial",
+    options: [
+      "Monarki Absolut",
+      "Republik Presidensial",
+      "Republik Parlementer",
+      "Monarki Konstitusional"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Siapa Presiden Idonesia Ke-4 Indonesia?",
+    answer: "Abdurrahman Wahid",
+    options: [
+      "Susilo Bambang Yudhoyono",
+      "Bj Habibie",
+      "Abdurrahman Wahid",
+      "Megawati Soekarno Putri",
+    ]
+  },
+    {
+    numb: 7,
+    question: "Julukan Kota Surabaya Adalah",
+    answer: "Kota Pahlawan",
+    options: [
+      "Kota Pahlawan",
+      "Kota Hujan",
+      "Kota Pempek",
+      "Kota Kembang",
+    ]
+  },
+    {
+    numb: 8,
+    question: "Sumpah Yang Diucapkan Oleh Gajah Mada Disebut",
+    answer: "Sumpah Palapa",
+    options: [
+      "Sumpah Serapah",
+      "Sumpah Palapa",
+      "Sumpah Kelapa",
+      "Sumpah Pocong",
+    ]
+  },
+    {
+    numb: 9,
+    question: "Kerajaan Samudra Pasai Berasal Dari Daerah",
+    answer: "Aceh",
+    options: [
+      "Aceh",
+      "Makassar",
+      "Maluku",
+      "Ambon",
+    ]
+  },
+    {
+    numb: 10,
+    question: "Siapakah Gubernur DIY (Daerah Istimewa Yogyakarta) Tahun 2024",
+    answer: "Sri Sultan Hamengku Buwono X",
+    options: [
+      "Sri Sultan Hamengku Buwono X",
+      "Sri Sultan Hamengku Buwono IX",
+      "Sri Sultan Hamengku Buwono VI",
+      "Sri Sultan Hamengku Buwono VII",
+    ]
+  },
+];
